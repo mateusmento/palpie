3cb571d75a3d86f9f6fc4ca90bfde399d477333a
Mateus Sarmento <mateus.sarmento@qikserve.com>
2023-10-05 17:51:11 -0300

    Add Field component

---
+++ app/src/components/Field.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+import Input from './Input.vue';
+import uniqid from 'uniqid';
+
+const props = defineProps<{
+  label: string;
+  name: string;
+}>();
+
+const fieldId = computed(() => uniqid(props.name + '-'));
+</script>
+
+<template>
+  <div class="field">
+    <label :for="fieldId">{{ label }}</label>
+    <Input v-bind="$attrs" :id="fieldId" :name="name" />
+  </div>
+</template>
