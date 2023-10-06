6f13c057b134645b9af5038547d80f6d61a69172
Mateus Sarmento <mateus.sarmento@qikserve.com>
2023-10-06 18:43:08 -0300

    Setup typeorm with docker database

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'palpie',
+      username: 'palpie',
+      password: 'palpie',
+      logging: 'all',
+      logger: 'advanced-console',
+      autoLoadEntities: true,
+    }),
+    ProductModule,
+  ],
   controllers: [],
   providers: [],
 })
--- api/src/product/product.module.ts
+++ api/src/product/product.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([])],
   controllers: [ProductsController],
 })
 export class ProductModule {}
