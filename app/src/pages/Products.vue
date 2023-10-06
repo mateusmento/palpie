1e152557ec9e0df8f80c53cc3b49f2c19e0ffbe2
Mateus Sarmento <mateus.sarmento@qikserve.com>
2023-10-06 16:00:30 -0300

    Add Products component

---
+++ app/src/pages/Products.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { useProductsStore } from '@/domain/products/products.store';
+import { onMounted } from 'vue';
+
+const productsStore = useProductsStore();
+
+onMounted(() => {
+  productsStore.fetchProducts();
+});
+</script>
+
+<template>
+  <section class="flex rows-lg card-lg">
+    <h2>Products</h2>
+    <div class="list flex rows-sm">
+      <div
+        v-for="product of productsStore.products"
+        :key="product.id"
+        class="list-item flex cols-sm"
+      >
+        <div class="mr-auto">{{ product.title }}</div>
+        <div>${{ product.price }}</div>
+        <div>{{ product.quantity }} units</div>
+      </div>
+    </div>
+  </section>
+</template>
