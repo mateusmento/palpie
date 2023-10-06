072b4fc53f2262d2f3fc7451c81264b23d0d53b6
Mateus Sarmento <mateus.sarmento@qikserve.com>
2023-10-06 16:00:08 -0300

    Add find all to ProductsApi

--- app/src/domain/products/products.api.ts
+++ app/src/domain/products/products.api.ts
@@ -2,6 +2,10 @@ import axios from '@/core/axios';
 import type { Product } from './product.type';
 
 export class ProductsApi {
+  findAll() {
+    return axios.get('/products').then((res) => res.data);
+  }
+
   create(data: Partial<Product>) {
     return axios.post('/products', data).then((res) => res.data);
   }
