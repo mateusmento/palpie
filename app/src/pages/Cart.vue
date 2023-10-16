9c9578e02f73000f08cc402761eca6d9c4ad42df
Mateus Sarmento <mateus.sarmento@qikserve.com>
2023-10-16 18:07:27 -0300

    Render list of CartItem in Cart

--- app/src/pages/Cart.vue
+++ app/src/pages/Cart.vue
@@ -1,9 +1,22 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import PalCartItem from '@/components/PalCartItem.vue';
+import { useCartStore } from '@/domain/cart/cart.store';
+
+const cart = useCartStore();
+</script>
 
 <template>
-  <section class="card-lg">
+  <section class="flex rows-lg card-lg">
     <h2>Cart</h2>
+    <div class="list grid rows-lg">
+      <PalCartItem v-for="cartItem of cart.items" :key="cartItem.id" :cart-item="cartItem" />
+    </div>
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.list {
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  grid-auto-rows: 200px;
+}
+</style>
