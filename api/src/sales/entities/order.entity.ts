63eba653499b5392066fb696102a15d6b9f85315
Mateus Sarmento <mateus.sarmento@qikserve.com>
2023-10-23 07:20:44 -0300

    Add Order and OrderItem entities

---
+++ api/src/sales/entities/order.entity.ts
@@ -0,0 +1,26 @@
+import { Column, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { SaleProduct } from './sale-product.entity';
+
+export class OrderItem {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  productId: number;
+  @ManyToOne(() => SaleProduct)
+  product: SaleProduct;
+
+  @Column()
+  quantity: number;
+
+  @ManyToOne(() => Order)
+  order: Order;
+}
+
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @OneToMany(() => OrderItem, (item) => item.order)
+  items: OrderItem[];
+}
--- api/src/sales/sales.module.ts
+++ api/src/sales/sales.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SaleProduct } from './entities/sale-product.entity';
+import { Order, OrderItem } from './entities/order.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SaleProduct])],
+  imports: [TypeOrmModule.forFeature([SaleProduct, Order, OrderItem])],
 })
 export class SalesModule {}
