e26a75b83ea9544ff58e08073a2833e9df6a530e
Mateus Sarmento <mateus.sarmento@qikserve.com>
2023-10-23 07:26:00 -0300

    Add listener to InventoryMdoule

--- api/src/inventory/inventory.module.ts
+++ api/src/inventory/inventory.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductAvailability } from './entities/product-availability.entity';
 import { AdjustAvailabilityCommand } from './commands/adjust-availability.command';
+import { ProductCreatedListener } from './listeners/product-created.listener';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ProductAvailability])],
-  providers: [AdjustAvailabilityCommand],
+  providers: [AdjustAvailabilityCommand, ProductCreatedListener],
 })
 export class InventoryModule {}
