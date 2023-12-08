d03f8f833f60bd7d4ec1c26532a320d73c7a9f50
Mateus Sarmento <mateus.sarmento@qikserve.com>
2023-12-08 20:10:02 -0300

    Use PALPIE_API_HOST env variable

--- app/src/core/axios.ts
+++ app/src/core/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const host = import.meta.env.VITE_PALPIE_API_HOST;
+
 export default axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: `http://${host}`,
 });
